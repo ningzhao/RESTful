63c06f63d525c7be94f7c823e79a656f9158b516
Ning <ningzhao@nings-mbp.attlocal.net>
2017-11-19 12:00:51 -0800

    change to localhost

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // @Ning created 11/12/2017
 
 var express = require('express'), app = express();
-var host = process.env.IP || '18.221.143.222',  port = process.env.port || '3000';
+var host = process.env.IP || '0.0.0.0',  port = process.env.port || '3000';
 var mongoose = require('mongoose');
 var RoadsignSchema = require('./api/models/roadsignModel');
 var bodyParser = require('body-parser');
