8e6a8229e1b10c6dbee075151044429450fba451
Ning <ningzhao@nings-mbp.attlocal.net>
2017-11-19 11:53:15 -0800

    port fixing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // @Ning created 11/12/2017
 
 var express = require('express'), app = express();
-var host = process.env.IP || '18.221.143.222',  port = process.env.port || '80';
+var host = process.env.IP || '18.221.143.222',  port = process.env.port || '3000';
 var mongoose = require('mongoose');
 var RoadsignSchema = require('./api/models/roadsignModel');
 var bodyParser = require('body-parser');
