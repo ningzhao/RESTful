45ee411eae43e3b7d7d2c0a50e699220e85234dd
Ning <ningzhao@nings-mbp.attlocal.net>
2017-11-19 10:37:36 -0800

    host change to aws

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // @Ning created 11/12/2017
 
-var express = require('express'), app = express(), port = process.env.port || 8080;
+var express = require('express'), app = express();
+var host = process.env.IP || 'ec2-18-221-143-222.us-east-2.compute.amazonaws.com',  port = process.env.port || 8080;
 var mongoose = require('mongoose');
 var RoadsignSchema = require('./api/models/roadsignModel');
 var bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-var db = mongoose.connect('mongodb://localhost/bmw');
+var db = mongoose.connect('mongodb://"ec2-18-221-143-222.us-east-2.compute.amazonaws.com:8080"/bmw');
 
 db.connection.on("error", function(err) {
   console.log("failed in connection to database: ", err);
